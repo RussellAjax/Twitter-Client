cef9030f1bc1118481b66c1f7e7445afb8345435
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2020-05-30 08:52:21 +0700

    Add Python app file

--- Twitter-Client/app.py
+++ Twitter-Client/app.py
@@ -1,1 +1,20 @@
+import requests
+import json
+from requests_oauthlib import OAuth1
+
+url = 'https://api.twitter.com/1.1/statuses/update.json'
+ask_tweet = input("What's happening today?\n")
+tweet = {'status': ask_tweet}
+
+with open("data\\twitter_credential.json") as json_file:
+    auth_codes = json.load(json_file)
+
+auth = OAuth1(auth_codes['CONSUMER_KEY'], auth_codes['CONSUMER_SECRET'], auth_codes['ACCESS_TOKEN'], auth_codes['ACCESS_SECRET'])
+
+r = requests.post(url, params = tweet, auth=auth)
+#print(r.status_code)
+if r.status_code == 200:
+    print("Your Tweet was successfully sent!")
+else:
+    print("Your Tweet was not send!")
 
